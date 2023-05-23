293ad778ccd534175c10dcb5e06d0f72e72498e8
samoelmandrade <101720444+SamoelSLC@users.noreply.github.com>
2023-05-23 18:31:59 -0300

    main

--- pandas/2 - Data Frame/DataFrame/06 - deletando linhas e colunas.py
+++ pandas/2 - Data Frame/DataFrame/06 - deletando linhas e colunas.py
@@ -13,3 +13,23 @@ print('Somente linhas com dados')
 deletandoLinhasEmBranco = dataFrameDados.dropna()
 print(deletandoLinhasEmBranco)
 print('\n')
+
+'''print('deletando coluna produto')
+del deletandoLinhasEmBranco["Produto"]
+print(deletandoLinhasEmBranco)
+print('\n')'''
+
+print('deletando linhas produto e data venda')
+deletandoDuasColunas = deletandoLinhasEmBranco.drop(columns=["Produto" , "Data Venda"])
+print(deletandoDuasColunas)
+print('\n')
+print('deletando linha 2')
+#axis = eixo(1 - coluna , 0 - linha)
+excluirLinha3 = deletandoDuasColunas.drop(2,axis=0)
+print(excluirLinha3)
+print('\n')
+print('deletando linha 1 ')
+#axis = eixo(1 - coluna , 0 - linha)
+excluirLinha2 = deletandoDuasColunas.drop(1,axis=0)
+print(excluirLinha2)
+print('\n')
